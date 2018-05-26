e25a5bf98211a8d78681e0c61442aa3fade07acb
linmiss <linmiss8128@gmail.com>
2018-05-26 23:25:25 +0800

    over proof of work code

--- src/main.go
+++ src/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func main() {
@@ -12,8 +13,12 @@ func main() {
 
 	for index, block := range bc.blocks {
 		fmt.Println("----------------------", index, "block")
-		fmt.Printf("PreBlockHash: %x\n", block.PreBlockHash)
+		fmt.Printf("Prev hash: %x\n", block.PreBlockHash)
 		fmt.Printf("Data: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
+		fmt.Printf("Nonce: %x\n", block.Nonce)
+		pow := NewProofOfWork(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
 	}
 }
